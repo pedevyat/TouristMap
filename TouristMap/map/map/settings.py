707a0bdc4ca0587c1d70bda9d5a1c345d5b670f1
Kostya <114941602+pedevyat@users.noreply.github.com>
2026-03-29 22:35:41 +0300

    init map (not work)
    Добавил интеграцию карты, но пока не работает...

--- TouristMap/map/map/settings.py
+++ TouristMap/map/map/settings.py
@@ -24,9 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&mv90tg!+lkgk=@h416bof30caeu7na!5_z#2wuuede6bu#!1_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
+INSTALLED_APPS = [
+    'django.contrib.admin',
+]
 
 
 # Application definition
